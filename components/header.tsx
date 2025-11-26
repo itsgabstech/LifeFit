6487fe0d1d304220ca3e3f26b1e0311c1b20dfb6
itsgabstech <gabrielavberto@gmail.com>
2025-11-26 17:04:36 -0300

    última versão do projeto

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,3 @@
-// PESSOA 1 - PR 1: Complete this component
-// TODO: Implementar header com nome do usuário e logout
 
 interface HeaderProps {
   userName: string;
@@ -7,7 +5,6 @@ interface HeaderProps {
 }
 
 export default function Header({ userName, onLogout }: HeaderProps) {
-  // TODO: Add header content here
   return (
     <div className="bg-slate-900 border-b border-slate-800 sticky top-0 z-10">
       <div className="px-4 py-4 max-w-2xl mx-auto">
